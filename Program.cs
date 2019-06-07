fd03c855016b92a998ab53cc5068c63172bf3885
sameeakhtar <samee.y@amplelogic.com>
2019-06-07 17:05:27 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new Dictionary<string, string>() {{"message", "Hello World!"}};
+            var serialized = JsonConvert.SerializeObject(dictionary);
+            Console.WriteLine(serialized);
+        }
+    }
+}
